e068ffb2d9a179766759a348ce0ba895905ff178
buthed <temaTihonov@yandex.ru>
2021-11-13 17:53:47 +0300

    First commit

--- app/src/main/java/com/example/chooseyoufavorite/MainActivity.kt
+++ app/src/main/java/com/example/chooseyoufavorite/MainActivity.kt
@@ -2,31 +2,12 @@ package com.example.chooseyoufavorite
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.widget.ViewPager2
-import com.example.chooseyoufavorite.View.Adapters.MainAdapter
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
-
-const val ARG_OBJECT = "object"
 
 class MainActivity : FragmentActivity() {
-
-    private lateinit var adapter: MainAdapter
-    private lateinit var viewPager: ViewPager2
-    private lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tabNames = resources.getStringArray(R.array.categories)
-        adapter = MainAdapter(this)
-        viewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = adapter
-
-        tabLayout = findViewById(R.id.tab_layout)
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = tabNames[position]
-        }.attach()
     }
 }
--- app/src/main/java/com/example/chooseyoufavorite/Models/Repository/Repository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chooseyoufavorite.Models.Repository
-
-import com.example.chooseyoufavorite.Models.CategoryList
-
-interface Repository {
-    fun getCategoryFromServer(movie_id: Int): CategoryList
-    fun getCategoryFromLocalStorageMovie(): List<CategoryList>
-    fun getCategoryFromLocalStorageMusic(): List<CategoryList>
-}
