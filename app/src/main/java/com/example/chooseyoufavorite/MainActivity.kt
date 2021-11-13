d329b55707f0c97d54aa040117ec3dcab0d4ff8e
buthed <temaTihonov@yandex.ru>
2021-11-13 18:32:53 +0300

    With ViewBinding abstract class

--- app/src/main/java/com/example/chooseyoufavorite/MainActivity.kt
+++ app/src/main/java/com/example/chooseyoufavorite/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.chooseyoufavorite
 
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chooseyoufavorite.databinding.ActivityMainBinding
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 
+    override fun onResume() {
+        super.onResume()
+
+        supportFragmentManager.beginTransaction().replace(R.id.container,MainFragment.newInstance()).commit()
     }
 }
