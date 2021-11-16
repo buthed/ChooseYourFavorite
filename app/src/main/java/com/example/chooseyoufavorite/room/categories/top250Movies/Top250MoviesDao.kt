0c15af91f81524913b6810ac1028e08798f61c13
buthed <temaTihonov@yandex.ru>
2021-11-16 19:01:56 +0300

    With room and retrofin

--- app/src/main/java/com/example/chooseyoufavorite/room/categories/top250Movies/Top250MoviesDao.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chooseyoufavorite.room.categories.top250Movies
-
-import androidx.room.Query
-
-interface Top250MoviesDao {
-
-    @Query("SELECT * FROM Top250MoviesEntity")
-    fun selectAll(): List<Top250MoviesEntity>
-}
