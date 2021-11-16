f10aaddb9c585aad6a276339d78ca9ed26ddad0c
buthed <temaTihonov@yandex.ru>
2021-11-16 16:02:03 +0300

    21.11.16

--- app/src/main/java/com/example/chooseyoufavorite/view/CategoriesFragment.kt
+++ app/src/main/java/com/example/chooseyoufavorite/view/CategoriesFragment.kt
@@ -1,30 +1,9 @@
 package com.example.chooseyoufavorite.view
 
-
-import android.os.Bundle
-import android.view.View
 import com.example.chooseyoufavorite.ViewBindingFragment
 import com.example.chooseyoufavorite.databinding.FragmentCategoriesBinding
-import com.example.chooseyoufavorite.view.Movies.details.CinemaFragment
-
 
 class CategoriesFragment : ViewBindingFragment<FragmentCategoriesBinding>(FragmentCategoriesBinding::inflate) {
-
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//
-//        binding.btn1.setOnClickListener {
-//            activity?.supportFragmentManager?.apply {
-//                beginTransaction()
-//                    .add(binding.frameCat, CinemaFragment.newInstance(Bundle().apply {
-//                        putParcelable(CinemaFragment.BUNDLE_EXTRA, "220")
-//                    }))
-//                    .addToBackStack("")
-//                    .commitAllowingStateLoss()
-//            }
-//        }
-//    }
 
     companion object{
         fun newInstance(): HomeFragment {
