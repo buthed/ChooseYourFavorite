35469686df828c84f7d41dfb9100898963b93375
buthed <temaTihonov@yandex.ru>
2021-11-17 02:06:04 +0300

    With adapter in categories

--- app/src/main/java/com/example/chooseyoufavorite/MainActivity.kt
+++ app/src/main/java/com/example/chooseyoufavorite/MainActivity.kt
@@ -9,10 +9,13 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.chooseyoufavorite.view.HomeFragment
 import com.example.chooseyoufavorite.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mDatabase: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +27,24 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, HomeFragment.newInstance())
                 .commit()
         }
+        init()
+    }
 
+    private fun init() {
         //Initialize the bottom navigation view
         //Create bottom navigation view object
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         val navController = findNavController(R.id.container)
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navHome,
-            R.id.navCategories,
-            R.id.navHome))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navHome,
+                R.id.navCategories,
+                R.id.navHome
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
+        //Initialize Firebase
+        mDatabase = FirebaseDatabase.getInstance().getReference()
     }
 }
