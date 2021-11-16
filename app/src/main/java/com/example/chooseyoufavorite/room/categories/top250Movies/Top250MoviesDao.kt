b765e02674088c1e7b3f0dd4fc8b49c2a11c2bf0
buthed <temaTihonov@yandex.ru>
2021-11-16 23:40:53 +0300

    Fix room

--- app/src/main/java/com/example/chooseyoufavorite/room/categories/top250Movies/Top250MoviesDao.kt
+++ app/src/main/java/com/example/chooseyoufavorite/room/categories/top250Movies/Top250MoviesDao.kt
@@ -1,7 +1,9 @@
 package com.example.chooseyoufavorite.room.categories.top250Movies
 
+import androidx.room.Dao
 import androidx.room.Query
 
+@Dao
 interface Top250MoviesDao {
 
     @Query("SELECT * FROM Top250MoviesEntity")
--- app/src/main/java/com/example/chooseyoufavorite/room/categories/topHorrors/TopHorrorsDao.kt
+++ app/src/main/java/com/example/chooseyoufavorite/room/categories/topHorrors/TopHorrorsDao.kt
@@ -1,8 +1,9 @@
 package com.example.chooseyoufavorite.room.categories.topHorrors
 
+import androidx.room.Dao
 import androidx.room.Query
 
-
+@Dao
 interface TopHorrorsDao {
 
     @Query("SELECT * FROM TopHorrorsEntity")
